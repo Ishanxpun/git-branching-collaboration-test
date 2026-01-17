69020d02aa985b65d080d146f9c364291e19d1d2
Gamma <221785314+Coder-Gamma@users.noreply.github.com>
2026-01-17 19:05:46 +0545

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
     <div>
-      <nav className='flex h-30 w-screen bg-black text-white justify-between px-4'>
-         
+      <nav className="flex h-30 w-screen bg-black text-white justify-between px-4">
+        <select>
+          <option>Nirdosh</option>
+          <option value="">Ishanx</option>
+        </select>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
