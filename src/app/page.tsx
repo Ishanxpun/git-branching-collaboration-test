11470f2056bec54702bf85fee3f08503fe3e33d0
Ishanxpun <punishan500@gmail.com>
2026-01-17 18:51:30 +0545

    nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <nav className='flex h-30 w-screen bg-black text-white justify-between px-4'>
+         
+      </nav>
+    </div>
   )
 }
 
